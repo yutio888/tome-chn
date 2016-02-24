35715cc9d6849155afed248b41aa0e83a79b9897
flyingclouds <yutio888@mail.ustc.edu.cn>
2016-02-24 21:46:42 +0800

    翻译Psionic技能 补上steam pool技能
    Signed-off-by: flyingclouds <yutio888@mail.ustc.edu.cn>

--- superload/data-orcs/talents/psionic/action-at-a-distance.lua
+++ superload/data-orcs/talents/psionic/action-at-a-distance.lua
@@ -2,44 +2,44 @@ local _M = loadPrevious(...)
 
 registerTalentTranslation{
 	id = "T_CONDENSATE",
-	name = "Condensate",
+	name = "冷凝",
 	info = function(self, t)
 		local damage = t.getDamage(self, t)
-		return ([[Condensate hot steam around your foes in radius %d, burning them for %0.2f fire damage and applying the wet effect for 4 turns, halving their stun resistances.
-		The damage will increase with your Mindpower.]]):
+		return ([[在 你 敌 人 身 边 的 %d 码 内 冷 凝 热 蒸 汽。 对 他 们 造 成 %0.2f 火 焰 伤 害 并 浸 湿 他 们 4 回 合。 被 浸 湿 的 敌 人 的 震 撼 抗 性 会 减 半。
+		伤 害 会 随 着 你 的 精 神 强 度 增 加。]]):
 		format(self:getTalentRadius(t), damDesc(self, DamageType.FIRE, damage))
 	end,}
 
 registerTalentTranslation{
 	id = "T_SOLIDIFY_AIR",
-	name = "Solidify Air",
+	name = "固化空气",
 	info = function(self, t)
 		local damage = t.getDamage(self, t)
-		return ([[You concentrate your will in a cone in front of you, condensing the air into a tangible, solid form.
-		Any creatures caught inside take %0.2f physical damage.
-		Any places with no creatures will be filled with solid air, blocking the way for %d turns.
-		The damage will increase with your Mindpower.]]):
+		return ([[你 集 中 精 神 力 把 你 前 方 的 锥 形 空 间 内 的 空 气 凝 聚 成 固 态。
+		任 何 陷 入 其 中 的 生 物 都 会 受 到 %0.2f 的 物 理 伤 害。
+		任 何 没 被 生 物 占 据 的 地 方 会 被 固 化 空 气 占 满， 阻 路 %d 回 合。
+		伤 害 会 随 着 你 的 精 神 强 度 增 加。]]):
 		format(damDesc(self, DamageType.PHYSICAL, damage), t.getDur(self, t))
 	end,}
 
 registerTalentTranslation{
 	id = "T_SUPERCONDUCTION",
-	name = "Superconduction",
+	name = "超导",
 	info = function(self, t)
 		local damage = t.getDamage(self, t)
-		return ([[Call a streak of lightning on your target, dealing %0.2f to %0.2f lightning damage.
-		If it is wet the lightning propagates to all foes in radius %d, doing the same damage to each.
-		All affected foes are seared for 4 turns, reducing their fire resistance by %d%% and and mind save by %d.
-		The damage will increase with your Mindpower.]]):
+		return ([[召 唤 一 道 闪 电 劈 向 你 的 目 标， 造 成 %0.2f 到 %0.2f 闪 电 伤 害。
+		如 果 目 标 被 浸 湿 了， 那 么 闪 电 扩 散， 对 半 径 %d 码 内 的 所 有 单 位 造 成 同 样 的 伤 害。
+		所 有 被 劈 中 的 单 位 都 会 被 烧 焦 4 回 合， 降 低 他 们 的 火 焰 抗 性 %d%% 和 精 神 豁 免 %d。
+		伤 害 会 随 着 你 的 精 神 强 度 增 加。]]):
 		format(damDesc(self, DamageType.LIGHTNING, damage) / 3, damDesc(self, DamageType.LIGHTNING, damage), self:getTalentRadius(t), t.getSearing(self, t), t.getSearing(self, t))
 	end,}
 
 registerTalentTranslation{
 	id = "T_NEGATIVE_BIOFEEDBACK",
-	name = "Negative Biofeedback",
+	name = "负反馈",
 	info = function(self, t)
-		return ([[Any time your deal damage with a psionic ability you incur a negative biofeedback in your foes, stacking up to %d times for 5 turns.
-		Each stack reduces their physical save by %d, defense and armour by %d.
-		This effect may only occur once per turn.]]):
+		return ([[每 当 你 使 用 精 神 技 能 造 成 伤 害 时， 你 对 你 的 敌 人 施 加 一 个 负 反 馈， 能 叠 加 至 %d 层 并 持 续 5 回 合。
+		每 层 都 会 降 低 他 们 %d 的 物 理 豁 免 和 %d 的 防 御 与 护 甲。
+		这 个 效 果 每 回 合 只 能 触 发 一 次。]]):
 		format(t.getNb(self, t), t.getSave(self, t), t.getPower(self, t))
 	end,}
--- superload/data-orcs/talents/psionic/gestalt.lua
+++ superload/data-orcs/talents/psionic/gestalt.lua
@@ -2,42 +2,42 @@ local _M = loadPrevious(...)
 
 registerTalentTranslation{
 	id = "T_GESTALT",
-	name = "Gestalt",
+	name = "格式塔",
 	info = function(self, t)
-		return ([[You let your mind tap into your steam generators for energy, increasing your Mindpower in proportion to your steam level: %d when at full steam and 0 when at 0 steam.
-		Using a psionic talent will feedback into your generators, increasing your Steam power by %d for your next steamtech talent.
-		Using a steamtech talent will feedback into psionic focus, increasing your psi level by %d.
-		The effects increase with your Willpower.]]):
+		return ([[你 让 你 的 心 灵 从 你 的 蒸 汽 发 动 机 中 汲 取 能 量， 按 照 你 蒸 汽 的 比 例 增 加 你 的 精 神 强 度： 满 蒸 汽 时 %d 点， 0 蒸 汽 时 0 点。
+		使 用 精 神 技 能 会 反 馈 你 的 发 动 机， 为 你 的 下 一 个 蒸 汽 技 能 增 加 %d 点 蒸 汽 强 度。
+		使 用 蒸 汽 技 能 会 反 馈 你 的 精 神， 增 加 你 的 %d 点 你 的 超 能 力。
+		效 果 会 随 着 你 的 精 神 强 度 增 加。]]):
 		format(t.getMind(self, t), t.getSteam(self, t), t.getPsi(self, t))
 	end,}
 
 registerTalentTranslation{
 	id = "T_IMPROVED_GESTALT",
-	name = "Improved Gestalt",
+	name = "强化格式塔",
 	info = function(self, t)
 		local shield_power = t.getShieldPower(self, t)
-		return ([[When you use a steamtech talent while Gestalt is active you drain some residual power to form a psionic shield.
-		The shield forms for 3 turns and absorbs %d damage.
-		The effects increase with your Mindpower.]]):format(shield_power)
+		return ([[每 当 你 在 格 式 塔 激 活 状 态 中 使 用 蒸 汽 技 能 时， 你 会 吸 取 一 些 残 留 的 力 量	来 形 成 一 个 精 神 护 盾。
+		这 个 护 盾 持 续 3 回 合， 并 能 吸 收 %d 伤 害。
+		效 果 会 随 着 你 的 精 神 强 度 增 加。]]):format(shield_power)
 	end,}
 
 registerTalentTranslation{
 	id = "T_INSTANT_CHANNELING",
-	name = "Instant Channeling",
+	name = "瞬间引导",
 	info = function(self, t)
-		return ([[Instantly channel all of your remaining steam to replenish your psi energies and either enhance your active psionic damage shield or trigger a new one.
-		The (new or existing) shield duration is increased by 3 turns and its power is boosted by %d%% of the steam used.
-		You restore psi equal to %d%% of the steam used.
-		This talent requires Gestalt to be active and either an active psionic damage shield or Improved Gestalt off cooldown.]]):format(t.getPower(self, t), t.getPsi(self, t))
+		return ([[瞬 间 引 导 你 剩 余 的 所 有 蒸 汽 来 补 充 你 的 超 能 力 并 充 能 或 制 造 一 个 新 的 精 神 护 盾。
+		护 盾 的 持 续 时 间 会 增 加 3 回 合， 并 能 多 吸 收 %d%% 消 耗 的 蒸 汽 数 额 的 伤 害。
+		你 回 复 等 同 于 %d%% 所 消 耗 的 蒸 汽 数 额 的 超 能 力。
+		此 技 能 需 要 格 式 塔 在 激 活 状 态 且 有 一 个 精 神 护 盾 或 者 强 化 格 式 塔 不 在 冷 却 中。]]):format(t.getPower(self, t), t.getPsi(self, t))
 	end,}
 
 registerTalentTranslation{
 	id = "T_FORCED_GESTALT",
-	name = "Forced Gestalt",
+	name = "强力格式塔",
 	info = function(self, t)
-		return ([[Temporarily expand your mind to force your Gestalt upon your foes in a radius of 5. Up to %d foe(s) will be affected.
-		The Gestalt will drain each affected foe's powers (physical power, mind power, spell power and steam power) by %d for 5 turns.
-		Your own powers will be increased in return by the drained amount (reduced for each additional foe).
-		In addition for 5 turns you can sense creatures beyond your sight, even through walls in radius %d.
-		The effects improve with your Mindpower.]]):format(t.getNb(self, t), t.getPower(self, t), t.getSenseRadius(self, t))
+		return ([[暂 时 延 伸 你 的 心 灵 以 使 你 的 格 式 塔 笼 罩 你 周 围 半 径 5 码 内 的 敌 人， 最 多 可 影 响 %d 个 敌 人。
+		格 式 塔 会 吸 收 每 个 被 影 响 敌 人 的 力 量 （物 理 强 度， 精 神 强 度， 法 术 强 度， 蒸 汽 强 度） %d 回 合。
+		你 自 身 的 力 量 会 增 加 所 吸 取 的 数 额 （效 果 每 个 额 外 的 敌 人 都 会 衰 减）。
+		除 此 之 外， 在 5 回 合 内 你 可 以 超 脱 视 线 的 感 知 半 径 %d 码 内 的 生 物。
+		效 果 会 随 着 你 的 精 神 强 度 增 加。]]):format(t.getNb(self, t), t.getPower(self, t), t.getSenseRadius(self, t))
 	end,}

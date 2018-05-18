910662cf786f8b3978676b44f525d808c08210df
yutio888 <yutio888@mail.ustc.edu.cn>
2018-05-18 12:10:26 +0800

    add translation for nether

--- superload/data-cults/talents/demented/nether.lua
+++ superload/data-cults/talents/demented/nether.lua
@@ -2,60 +2,64 @@ local _M = loadPrevious(...)
 
 registerTalentTranslation{
 	id = "T_NETHERBLAST",
-	name = "Netherblast",
+	name = "虚空爆炸",
 	info = function(self, t)
 		local dam = t.getDamage(self,t)/2
 		local backlash = t.getBacklash(self,t)
-		return ([[Fire a burst of unstable void energy, dealing %0.2f darkness and %0.2f temporal damage to the target. The power of this spell inflicts entropic backlash on you, causing you to take %d damage over 8 turns. This damage counts as entropy for the purpose of Entropic Gift.
-		The damage will increase with your Spellpower.]]):
+		return ([[发 射 一 束 不 稳 定 的 虚 空 能 量，造 成 %0.2f 暗 影 %0.2f 时 空 伤 害 。
+		该 法 术 会 对 你 产 生 熵 能 反 冲， 在 8 回 合 内 造 成 %d 伤 害 。
+		伤 害 受 法 术 强 度 加 成 。]]):
 		format(damDesc(self, DamageType.DARKNESS, dam), damDesc(self, DamageType.TEMPORAL, dam), backlash)
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_RIFT_CUTTER",
-	name = "Rift Cutter",
+	name = "裂缝切割",
 	info = function(self, t)
-		return ([[Fire a beam of energy that rakes across the ground, dealing %0.2f darkness damage to enemies within and leaving behind an unstable rift. After 3 turns the rift detonates, dealing %0.2f temporal damage to adjacent enemies.
-		Targets cannot be struck by more than a single rift explosion at once.
-		The power of this spell inflicts entropic backlash on you, causing you to take %d damage over 8 turns. This damage counts as entropy for the purpose of Entropic Gift.
-		The damage will increase with your Spellpower.]]):
+		return ([[发 射 一 束 扫 射 大 地 的 能 量， 造 成 %0.2f 暗 影 伤 害， 并 产 生 不 稳 定 的 裂 缝。 3 回 合 后 裂 缝 湮 灭 并 对 周 围 敌 人 造 成 %0.2f 时 空 伤 害。
+		一 次 湮 灭 不 能 多 次 伤 害 同 一 目 标。
+		该 法 术 会 对 你 产 生 熵 能 反 冲， 在 8 回 合 内 造 成 %d 伤 害 。
+		伤 害 受 法 术 强 度 加 成 。]]):
 		format(damDesc(self, DamageType.DARKNESS, t.getDamage(self,t)), damDesc(self, DamageType.TEMPORAL, t.getDamage(self,t)), t.getBacklash(self, t))
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_SPATIAL_DISTORTION",
-	name = "Spatial Distortion",
+	name = "空间扭曲",
 	info = function(self, t)
 		local dam = t.getDamage(self,t)/2
 		local backlash = t.getBacklash(self,t)
 		local dur = t.getDuration(self,t)
 		local rad = self:getTalentRadius(t)
-		return ([[Briefly open a radius %d rift in spacetime that teleports those within to the targeted location. Enemies will take %0.2f darkness and %0.2f temporal damage.
-		The power of this spell inflicts entropic backlash on you, causing you to take %d damage over 8 turns. This damage counts as entropy for the purpose of Entropic Gift.
-		The damage will improve with your Spellpower.]]):format(rad, damDesc(self, DamageType.DARKNESS, dam), damDesc(self, DamageType.TEMPORAL, dam), backlash)
+		return ([[ 在 时 空 中 临 时 打 开 半 径 %d 的 裂 缝 ， 将 范 围 内 目 标 传 送 至 指 定 位 置 。
+		敌 人 将 受 到 %0.2f 暗 影 %0.2f 时 空 伤 害。
+		该 法 术 会 对 你 产 生 熵 能 反 冲， 在 8 回 合 内 造 成 %d 伤 害 。
+		伤 害 受 法 术 强 度 加 成 。]]):format(rad, damDesc(self, DamageType.DARKNESS, dam), damDesc(self, DamageType.TEMPORAL, dam), backlash)
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_HALO_OF_RUIN",
-	name = "Halo of Ruin",
+	name = "毁灭光环",
 	info = function(self, t)
-		return ([[Each time you cast a non-instant Demented spell, a nether spark begins orbiting around you for 10 turns, to a maximum of 5. Each spark increases your critical strike chance by %d%%, and on reaching 5 stars your next Nether spell will consume all sparks to empower itself:
-#PURPLE#Netherblast:#LAST# Becomes a deadly lance of void energy, piercing through enemies and dealing an additional %d%% damage over 5 turns.
-#PURPLE#Rift Cutter:#LAST# Those in the rift will be pinned for %d turns, take %0.2f temporal damage each turn, and the rift explosion has %d increased radius.
-#PURPLE#Spatial Distortion:#LAST# An Entropic Maw will be summoned at the rift's exit for %d turns, pulling in and taunting nearby targets with it's tendrils.
-The damage will increase with your Spellpower.  Entropic Maw stats will increase with level and your Magic stat.]]):
+		return ([[ 每 次 你 释 放 疯 狂 法 术 时， 一 次 虚 空 火 花 将 环 绕 在 你 周 围 10 回 合， 上 限 为 5 个。
+		每 个 火 花 增 加 你 %d%% 暴 击 率。 当 你 拥 有 5 个 火 花 时， 你 的 下 一 次 虚 空 法 术 将 消 耗 所 有 火 花 来 获 得 强 化 效 果 。
+#PURPLE#虚 空 爆 炸：#LAST# 成 为 穿 透 性 虚 空 能 量  ， 并 在 5 回 合 内 造 成 额 外 %d%% 伤 害 。
+#PURPLE#裂 缝 切 割:#LAST# 裂 缝 内 的 敌 人 将 定 身 %d 回合，每 回 合 受 到 %0.2f 时 空 伤 害 。 裂 缝 湮 灭 时 爆 炸 半 径 增 加 %d 。
+#PURPLE#空 间 扭 曲:#LAST# 裂 缝 出 口 处 产 生 一 个 持 续 %d 回 合 的 熵 之 胃， 能 用 触 须 拉 近 敌 人。
+伤 害 受 法 术 强 度 加 成 。
+熵 之 胃 的 属 性 受 等 级 和 魔 法 属 性 加 成 。]]):
 		format(t.getCrit(self,t), t.getSpikeDamage(self,t)*100, t.getPin(self, t), damDesc(self, DamageType.TEMPORAL, t.getRiftDamage(self,t)), t.getRiftRadius(self,t), t.getSpatialDuration(self,t))
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_GRASPING_TENDRILS",
-	name = "Grasping Tendrils",
+	name = "触须抓取",
 	info = function(self, t)
-		return ([[Grab a target and drag it to your side, dealing %d%% weapon damage and taunting it.]]):
+		return ([[抓 住 目 标 ，将 其 拉 到 身 边 ，造 成 %d%% 武 器 伤 害 并 嘲 讽 之。]]):
 		format(t.getDamage(self,t)*100)
 	end,
 }

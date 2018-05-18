68834bd4cf034abf10543991d79ce86871da68d7
xsy <yutio888@mail.ustc.edu.cn>
2018-05-18 10:21:34 +0800

    add translations for horrific-body and madness

--- superload/data-cults/talents/demented/horrific-body.lua
+++ superload/data-cults/talents/demented/horrific-body.lua
@@ -1,42 +1,41 @@
 local _M = loadPrevious(...)
 registerTalentTranslation{
 	id = "T_SHED_SKIN",
-	name = "Shed Skin",
+	name = "蜕皮",
 	info = function(self, t)
-		return ([[You shed the outer layer of your mutated skin and empower it to act as a damage shield for 7 turns.
-		The shield can absorb up to %d damage before it crumbles.
-		]]):format(t.getAbsorb(self, t))
+		return ([[你 蜕 去 表 层 变 异 皮 肤， 同 时 增 加 它 的 强 度， 令 其 在 7 回 合 内 吸 收 %d 伤 害 。]]):format(t.getAbsorb(self, t))
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_PUSTULENT_GROWTH",
-	name = "Pustulent Growth",
+	name = "脓包生长",
 	info = function(self, t)
-		return ([[Each time your shed skin looses %d%% of its max power or you take damage over 15%% of your maximum life a black putrescent pustule grows on your body for 5 turns.
-		Each pustule increases all your resistances by %d%%. You can have up to %d pustules at once.
-		Resistance scales with your Spellpower.]]):
+		return ([[每 次 你 的 蜕 皮 护 盾 损 失 %d%% 护 盾 量 或 者 你 受 到 超 过 15%% 最 大 生 命 值 的 伤 害 时， 一 个 黑 色 的 腐 败 脓 包 将 在 你 身 上 生 长 5 回 合。
+		每 个 脓 包 增 加 你 %d%% 全 体 伤 害 抗 性。
+		你 最 多 能 同 时 拥 有 %d 个 脓 包。
+		抗 性 受 法 术 强 度 加 成 。]]):
 		format(t.getCutoff(self, t), t.getResist(self, t), t.getMax(self, t))
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_PUSTULENT_FULMINATION",
-	name = "Pustulent Fulmination",
+	name = "脓包爆裂",
 	info = function(self, t)
-		return ([[You make all your putrescent pustules explode at once, splashing all creatures in radius %d with black fluids that deal %0.2f darkness damage per pustule and healing you for %0.1f per pustule.]]):
+		return ([[你 引 爆 身 上 所 有 脓 包， 产 生 黑 色 液 体 溅 射 到 半 径 %d 格 内 所 有 生 物上， 每 个 脓 包 造 成 %0.2f 暗 影 伤 害 并 治 疗 你 %0.1f 生命。]]):
 		format(self:getTalentRadius(t), damDesc(self, DamageType.DARKNESS, t.getDam(self, t)), t.getHeal(self, t))
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_DEFILED_BLOOD",
-	name = "Defiled Blood",
+	name = "污血",
 	info = function(self, t)
-		return ([[When you make your pustules explode you leave a pool of defiled blood on the ground for 5 turns.
-		Foes caught inside get assaulted by black tentacles every turn, dealing %d%% darkness tentacle damage and covering them in your black blood for 2 turns.
-		Creatures that hit you while covered in your blood heal you for %d%% of the damage done.
-		The healing received increases with your Spellpower.]]):
+		return ([[每 次 脓 包 爆 裂 时， 你 会 在 地 上 留 下 一 滩 持 续 5 回 合 的 污 血 。
+		范 围 内 的 敌 人 每 回 合 将 被 污 血 中 产 生 的 触 手 纠 缠， 受 到 %d%% 暗 影 触 手 伤 害 并 被 污 血 覆 盖 2 回 合 。
+		被 污 血 覆 盖 的 敌 人 击 中 你 时 ， 你 受 到 %d%% 相 应 伤 害 的 治 疗。
+		治 疗 系 数 受 法 术 强 度 影 响。]]):
 		format(t.getDam(self, t) * 100, t.getHeal(self, t))
 	end,
 }
--- superload/data-cults/talents/demented/madness.lua
+++ superload/data-cults/talents/demented/madness.lua
@@ -1,52 +1,51 @@
 local _M = loadPrevious(...)
 registerTalentTranslation{
 	id = "T_DARK_WHISPERS",
-	name = "Dark Whispers",
+	name = "黑暗低语",
 	info = function(self, t)
 		local dam = t.getDamage(self,t)
 		local stat = t.getPowerLoss(self,t)
 		local rad = self:getTalentRadius(t)
-		return ([[Terrible visions and maddening voices fill the minds of enemies within a radius %d area, inflicting %0.2f darkness damage each turn for 5 turns. In addition, this distraction will reduce physical, spell and mindpower of those affected by %d.
-The power loss caused by this spell can stack, to a maximum of %d powers.
-		The effect will increase with your Spellpower.]]):
+		return ([[令 半 径 %d 格 内 的 敌 人 的 心 灵 里 充 满 可 怕 的 幻 觉 和 疯 狂 的 低 语 ， 5 回 合 内 每 回 合 受 到 %0.2f 暗 影 伤 害 。 同 时 ， 该 效 果 将 降 低 其 物  理、  法 术 和精 神 强 度 %d 点 ， 该 效 果 可 叠 加 至 最 多 %d 点。
+		技 能 效 果 受 法 术 强 度 加 成 。]]):
 		format(rad, damDesc(self, DamageType.DARKNESS, dam), stat, stat*3)
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_HIDEOUS_VISIONS",
-	name = "Hideous Visions",
+	name = "惊骇幻象",
 	info = function(self, t)
 		local chance = t.getChance(self,t)
 		local dur = t.getDuration(self,t)
 		local damage = t.getDamageReduction(self,t)
-		return ([[Each time an enemy takes damage from Dark Whispers, there is a %d%% chance for one of their visions to manifest in an adjacent tile for %d turns. This vision takes no actions but the victim will deal %d%% reduced damage to all other targets until the vision is slain.
-		A target cannot have more than one hallucination at a time.]]):
+		return ([[每 次 敌 人 受 到 黑 暗 低 语 的 伤 害 时， 有 %d%% 几 率 在 视 野 内 产 生 持 续 %d 回 合 的 幻 象。 幻 象 不 能 行 动 ， 但 被 影 响 的 敌 人 在 幻 象 结 束 前 造 成 的 伤 害 降 低 %d%%。
+		同 一 敌 人 同 时 只 能 产 生  一 个 幻 象。]]):
 		format(chance, dur, damage)
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_SANITY_WARP",
-	name = "Sanity Warp",
+	name = "失智冲击",
 	info = function(self, t)
 		local dam = t.getDamage(self,t)
 		local radius = self:getTalentRadius(t)
-		return ([[When a hallucination from Hideous Visions is slain, it unleashes a psychic shriek dealing %0.2f darkness damage to enemies in radius %d.]]):
+		return ([[每 当 幻 象 被 消 灭 时， 它 将 释 放 心 灵 冲 击 ，对 %d 格 内 的 敌 人 造 成 %d0.2f 暗 影 伤 害 。 ]]):
 		format(damDesc(self, DamageType.DARKNESS, dam), radius)
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_CACOPHONY",
-	name = "Cacophony",
+	name = "心灵尖啸",
 	info = function(self, t)
 		local rad = self:getTalentRadius(t)
 		local dur = t.getDuration(self,t)
 		local dam = t.getDamage(self,t)*100
-		return ([[Raise your Dark Whispers in radius %d to a deafening crescendo for %d turns, applying another stack and drowning out all thought. 
-			Targets afflicted by Dark Whispers will have 20%% higher chance to spawn hallucinations, and each time they take damage from your Dark Whispers or Sanity Warp they will take an additional %d%% damage as temporal damage.
-		The damage will improve with your Spellpower.]]):format(rad, dur, dam)
+		return ([[ 使 %d 格 内 的 黑 暗 低 语 音 量 提 升 %d 回 合 ， 达 到 震 耳 欲 聋 的 地 步， 额 外 施 加 一 层 低 语 效 果， 同 时 干 扰 一 切 思 考 能 力。
+		被 黑 暗 低 语 影 响 的 目 标 产 生 幻 象 的 几 率 增 加 20%%， 每 次 受 到 黑 暗 低 语 或 失 智 冲 击 的 伤 害 时， 会 受 到 额 外 %d%% 时 空 伤 害。
+		伤 害 受 法 术 强 度 加 成 。]]):format(rad, dur, dam)
 	end,
 }
 

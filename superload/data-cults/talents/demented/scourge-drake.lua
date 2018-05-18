f2cec3dfcbdb98a894a253ea104bde490907c52a
BCSZSZ <liaowang586@hotmail.com>
2018-05-18 19:36:31 +0800

    Update scourge-drake.lua

--- superload/data-cults/talents/demented/scourge-drake.lua
+++ superload/data-cults/talents/demented/scourge-drake.lua
@@ -2,31 +2,31 @@ local _M = loadPrevious(...)
 
 registerTalentTranslation{
 	id = "T_TENTACLED_WINGS",
-	name = "Tentacled Wings",
+	name = "触手之翼",
 	info = function(self, t)
-		return ([[You project tentacles in a cone of radius %d in front of you.
-		Any foes caught inside is grappled by the tentacles and suffers %d%% weapon damage as blight, if the attack hits the creature is also pulled towards you.]]):
+		return ([[你向前方半径%d的锥形区域内发射触手。
+		任何在范围内的敌人将会被触手缠绕并受到属性为枯萎的%d%%点武器伤害，如果攻击命中，该生物还会被拉向你。]]):
 		format(self:getTalentRange(t), damDesc(self, DamageType.BLIGHT, t.getDamage(self, t) * 100))
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_DECAYING_GROUNDS",
-	name = "Decaying Grounds",
+	name = "腐朽之地",
 	info = function(self, t)
 		local damage = t.getDamage(self, t)
-		return ([[You blight a zone as a decaying ground for %d turns. All creatures inside take %0.2f blight damage per turn and have all their cooldowns increased by %d%% for 3 turns.
-		The damage will scale with the highest of your spell or mind power.]]):
+		return ([[你使一个区域枯萎，把它们变成腐朽之地，持续%d回合。所有在其中的生物每回合受到%0.2f点枯萎伤害，并且所有技能冷却时间增加%d%%回合，持续3回合。
+		伤害受你的法术强度或者精神强度两者中更高一方影响。]]):
 		format(t.getDuration(self, t), damDesc(self, DamageType.DARKNESS, damage), t.getPower(self,t))
 	end,
 }
 
 registerTalentTranslation{
 	id = "T_AUGMENT_DESPAIR",
-	name = "Augment Despair",
+	name = "扩大绝望",
 	info = function(self, t)
-		return ([[You target a single creature with all your hatred and insanity, augmenting its despair, increasing the duration of detrimental effects by %d turns and dealing %0.2f blight damage per detrimental effect (each effect deals 75%% of the previous one).
-		The damage will scale with the highest of your spell or mind power.]]):
+		return ([[你选取一个目标，将你的疯狂和仇恨灌注于它，扩大它的绝望。增加负面效果的持续时间%d回合，并且每有一个负面效果，造成%0.2f点枯萎伤害。(每个效果造成前一个效果75%%的伤害。)。
+		伤害受你的法术强度或者精神强度两者中更高一方影响。]]):
 		format(t.getDuration(self, t), damDesc(self, DamageType.BLIGHT, t.getDamage(self, t)))
 	end,
 }
@@ -34,11 +34,11 @@ registerTalentTranslation{
 
 registerTalentTranslation{
 	id = "T_MAGGOT_BREATH",
-	name = "Maggot Breath",
+	name = "蛆虫吐息",
 	info = function(self, t)
-		return ([[You breathe a wave of dead maggots in a cone of radius %d. Any target caught in the area will take %0.2f blight damage and be infected with a crippling disease for 10 turns.
-		Crippling disease slows creatures by %d%% and deals %0.2f blight damage per turn.
-		The damage will increase with your Magic, and the critical chance is based on your Spell crit rate.]]):
+		return ([[你向半径为%d的锥形区域内吐出一道由蛆虫尸体组成的波浪。任何在范围内的的目标会受到%0.2f 点枯萎伤害， 并且会被残废恶疾感染，持续10回合。
+		残废恶疾减慢目标%d%%，并且每回合造成%0.2f点枯萎伤害。.
+		伤害随法术强度增加而增加，暴击率基于法术暴击。]]):
 		format(self:getTalentRadius(t), damDesc(self, DamageType.BLIGHT, t.getDamage(self, t)), t.getSlow(self, t) * 100, damDesc(self, DamageType.BLIGHT, t.getDiseaseDamage(self, t)))
 	end,
 }

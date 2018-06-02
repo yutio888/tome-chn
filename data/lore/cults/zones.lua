9550eadd9701e691f6c86d1d0a798248272e95ca
StarSapphireX <806682153@qq.com>
2018-06-02 15:22:12 +0800

    一些剧情有关的手札

---
+++ data/lore/cults/zones.lua
@@ -0,0 +1,57 @@
+
+local CultsDLC = require "mod.class.CultsDLC"
+
+registerLoreTranslation{
+	id = "cults-entropic-void",
+	name = "熵之虚空",
+	lore = [[周围很冷，寒冷刺骨。环绕着你的非现实感似乎正在慢慢抽光你的一切，让你的身体僵硬，四肢麻木。这里是被熵支配的场所，但你还能够看到一束虚弱的微光在无尽的黑暗中坚强挺立。这份温暖的光芒给予了你力量，驱散了刺骨的寒冷。在这里，有一种反熵的力量，那是唯一战胜熵的东西。你可以看到，它正在顽强地在一场必败的战斗中抗争着。
+	当你重新取回你的感知的时候，你感受到，有一种什么东西正支配这这里。他的存在在虚空中咆哮，你的心中充满本能的恐惧。在这里发生的这场战斗和你所经历的任何一次都不同，可能也将是你的最后一场战斗。]],
+}
+
+registerLoreTranslation{
+	id = "cults-entropic-void-seen",
+	name = "熵的本质",
+	lore = [[熵的本质在你面前显现了。在它眼里，你是多么渺小。]],
+}
+
+
+registerLoreTranslation{
+	id = "cults-godfeaster-popup",
+	name = "啊啊啊啊啊啊啊",
+	lore = [[突然间地动山摇，地面裂开了！]],
+}
+
+registerLoreTranslation{
+	id = "cults-godfeaster-godflesh",
+	name = "神之肉",
+	lore = [[这个奇怪的橡胶状物质像是某种不明生物的血肉。在上面不时涌动着神秘的能量，充满不可思议的活力。你怀疑，是在那个蛆虫吞下了这个东西之后，才变成了吞噬你的怪物。]],
+}
+
+registerLoreTranslation{
+	id = "cults-occult-egress-1",
+	name = "出口 (1)",
+	lore = function() return [[当个冒险家本来应该是有趣的。你知道的，就像这样，到处走来走去，杀杀怪物，捡捡装备，把装备卖掉换成钱，然后再重复一遍。只要你能比别人先行动一步，你总能找到四处掠夺的机会。但是有时候，那些人喜欢把战利品锁在愚蠢的谜题里面，就像这个一样。我坐在这里，不停地尝试着不同的组合。我日夜不休，不停尝试，毫无结果。这种愚蠢的东西根本毫无意义！大部分人如果想要设计个谜题，总得弄点线索吧？可我什么都没找到！
+
+所以，我唯一的办法就是不停尝试不同的组合，直到奏效为止。我在日志上记录下那些组合，然后给那些组合打钩或者打叉。这样我就知道哪些组合是有用的，而哪些不是。让我做这种该死的工作，最好能给我弄点好点的奖励……
+
+#{italic}#(这个列表上列举着的几乎所有的组合都打着叉，除了最后一行以外。令人奇怪的是，这个组合既没有打钩也没有打叉。也许是他已经没有机会去试试这个组合好不好了？)#{normal}#
+]]..CultsDLC.effectGlyphsSequence("EGRESS_SPLATTER") end,
+}
+
+registerLoreTranslation{
+	id = "cults-occult-egress-2",
+	name = "the egress (2)",
+	lore = [[这是什么鬼谜题！我终于输入了正确的组合，结果他把我扔到了世界遗忘的角落！我甚至不知道周围的东西是怎么建造出来的，真见鬼！为什么我要倒这种霉！我本来以为我是第一个到的，可以有机会搜刮财宝了…现在呢？我在这里到处游荡，结果连出口都找不到！这个地方简直就是就是为了困死我才设计的！]],
+}
+
+registerLoreTranslation{
+	id = "cults-ft-grung",
+	name = "禁忌之书《画廊一瞥》",
+	lore = [[这是一本有插图的书！]],
+}
+
+registerLoreTranslation{
+	id = "cults-ft-illusory-castle",
+	name = "禁忌之书《虚幻城堡》",
+	lore = [[这是一本有插图的书！]],
+}

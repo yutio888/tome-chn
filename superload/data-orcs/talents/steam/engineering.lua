da13744019700d954e7b48d492b944150949313b
starsapphirex <806682153@qq.com>
2019-11-20 15:35:32 +0800

    T_EMERGENCY_STEAM_PURGE

--- superload/data-orcs/talents/steam/engineering.lua
+++ superload/data-orcs/talents/steam/engineering.lua
@@ -2,7 +2,7 @@ local _M = loadPrevious(...)
 
 registerTalentTranslation{
 	id = "T_EMERGENCY_STEAM_PURGE",
-	name = "紧急蒸汽净化",
+	name = "紧急蒸汽排出",
 	info = function(self, t)
 		return ([[你打开所有蒸汽阀，释放半径 %d 的蒸汽冲击波，造成 %0.2f 火焰伤害。（这一技能无法暴击）
 		若你有至少 35 点蒸汽，气体的温度将变得极高，能烧伤感知器官，令受影响的生物目盲 %d 回合。 

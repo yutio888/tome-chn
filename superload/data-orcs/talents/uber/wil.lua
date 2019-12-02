35dbe40b60e519c6eb9ab2d9499a1dfc13bb92f1
starsapphirex <806682153@qq.com>
2019-12-02 22:56:00 +0800

    T_RANGE_AMPLIFICATION_DEVICE

--- superload/data-orcs/talents/uber/wil.lua
+++ superload/data-orcs/talents/uber/wil.lua
@@ -6,7 +6,7 @@ registerTalentTranslation{
 	require_special_desc ="光照范围为10或以上", 
 	info = function(self, t)
 		return ([[启动一个特殊的聚焦装置来使你的所有远程魔法和精神技能射程延长 3 （仅对射程至少为 2 的技能生效，且上限为 1 0 ）。
-		使用这个装置非常的费力，启动时会增加 60%% 疲劳值。]])
+		使用这个装置非常的费力，启动时会增加 20%% 疲劳值。]])
 		:format()
 	end,
 }
